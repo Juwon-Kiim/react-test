d0333274836acc19f36ef8db50a7669f567d7941
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-05-29 21:11:54 +0900

    use Hooks

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,12 +8,13 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <NotificationList />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Accommodate />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
